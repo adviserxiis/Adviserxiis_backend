0645678698f4520fdf93df368e23ec8849361cc5
ajikyasharma <ajikyasharma24@gamil.com>
2024-08-31 18:30:16 +0530

    updated create post route

--- Controllers/PostController.js
+++ Controllers/PostController.js
@@ -235,7 +235,7 @@ const createPost = async (req, res) => {
 
 
 
-      res.status(200).json({ message: 'Video uploaded and data saved successfully' });
+      res.status(200).json({ message: 'Video uploaded and data saved successfully' , postData});
   } catch (error) {
       console.error('Error during video upload:', error);
       res.status(500).json({ error: 'Something went wrong. Please try again later.' });
